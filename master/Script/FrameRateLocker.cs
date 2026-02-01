b5ca6f6729353336fd1c92a71086ca3f17c004e3
Milate <milanaryal57@gmail.com>
2026-02-01 10:06:48 +0545

    Auto Refresh Rate used for fps

--- master/Script/FrameRateLocker.cs
+++ master/Script/FrameRateLocker.cs
@@ -4,14 +4,19 @@ public class FrameRateLocker : MonoBehaviour
 {
     void Awake()
     {
-    //     // 1. Disable VSync (Essential: Unity ignores targetFrameRate if VSync is on)
-    //     QualitySettings.vSyncCount = 0;
+        //     // 1. Disable VSync (Essential: Unity ignores targetFrameRate if VSync is on)
+        //     QualitySettings.vSyncCount = 0;
 
-    //     // 2. Lock the frame rate to 60
-    //     Application.targetFrameRate = 60;
-    // }
+        //     // 2. Lock the frame rate to 60
+        //     Application.targetFrameRate = 60;
+        // }
 
-    QualitySettings.vSyncCount = 1;
-    Application.targetFrameRate = -1;
+        int refreshRate = (int)
+           System.Math.Round(
+               Screen.currentResolution.refreshRateRatio.value
+           );
+
+        Application.targetFrameRate = refreshRate;
+        QualitySettings.vSyncCount = 0;
     }
 }
