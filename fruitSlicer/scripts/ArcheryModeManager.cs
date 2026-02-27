7a47b4b130cd144983a0c45b21deefdb86c0769b
Milate <milanaryal57@gmail.com>
2026-02-27 12:18:40 +0545

    minor changes

--- fruitSlicer/scripts/ArcheryModeManager.cs
+++ fruitSlicer/scripts/ArcheryModeManager.cs
@@ -3,17 +3,10 @@ using UnityEngine;
 public class ArcheryModeManager : MonoBehaviour
 {
 
-    public float spawnDelay = 5f;
-
-    public float specialSpawnDelay = 12f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        ArcheryFruitSpawner fs = ArcheryFruitSpawner.instance;
-        fs.spawnDelay = spawnDelay;
-        fs.specialSpawnDelay = specialSpawnDelay;
-
         ModeManager mm = ModeManager.Instance;
         mm.currentMode = GameMode.Archery;
     }
