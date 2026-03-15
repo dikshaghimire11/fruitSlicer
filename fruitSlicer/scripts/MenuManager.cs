c6731ab61622e18b65e165b0e8a5f14014240c29
Dilate-byte <dikshaghimire565@gmail.com>
2026-03-15 12:42:55 +0545

    minor

--- fruitSlicer/scripts/MenuManager.cs
+++ fruitSlicer/scripts/MenuManager.cs
@@ -104,15 +104,15 @@ public class MenuManager : MonoBehaviour
             SoundManager.instance.PlayMenuMusic();
         }
 
-        // bool hasShownNotice = PlayerPrefs.GetInt("ShownNotice", 0) == 1;
+        bool hasShownNotice = PlayerPrefs.GetInt("ShownNotice", 0) == 1;
         // bool hasShownModeAddedNotice = PlayerPrefs.GetInt("ShownModeAddedNotice", 0) == 1;
 
-        // if (!hasShownNotice)
-        // {
-        //     StartCoroutine(MoveRoutine(new Vector2(-0.35f, -0.87f), 1, 1, noticeCharacter));
-        //     noticePanel.SetActive(true);
+        if (!hasShownNotice)
+        {
+            StartCoroutine(MoveRoutine(new Vector2(-0.35f, -0.87f), 1, 1, noticeCharacter));
+            noticePanel.SetActive(true);
 
-        // }
+        }
         // if (hasShownNotice && !hasShownModeAddedNotice)
         // {
         //     StartCoroutine(MoveRoutine(new Vector2(-0.35f, -0.87f), 1, 3, modeAddedNoticeCharacter));
@@ -139,17 +139,17 @@ public class MenuManager : MonoBehaviour
     //     PlayerPrefs.Save();
     //     archeryMode();
     // }
-    // public void CloseNoticePanel()
-    // {
-    //     if (SoundManager.instance != null)
-    //     {
-    //         SoundManager.instance.PlayButtonClickSound();
-    //     }
-    //     StartCoroutine(MoveRoutine(new Vector2(-2.67f, -0.87f), 1, 2, noticeCharacter));
-    //     noticePanel.SetActive(false);
-    //     PlayerPrefs.SetInt("ShownNotice", 1);
-    //     PlayerPrefs.Save();
-    // }
+    public void CloseNoticePanel()
+    {
+        if (SoundManager.instance != null)
+        {
+            SoundManager.instance.PlayButtonClickSound();
+        }
+        StartCoroutine(MoveRoutine(new Vector2(-2.67f, -0.87f), 1, 2, noticeCharacter));
+        noticePanel.SetActive(false);
+        PlayerPrefs.SetInt("ShownNotice", 1);
+        PlayerPrefs.Save();
+    }
     // public void tryNowButton()
     // {
     //     if (SoundManager.instance != null)
@@ -163,49 +163,49 @@ public class MenuManager : MonoBehaviour
     //     archeryMode();
     // }
 
-    // private IEnumerator MoveRoutine(Vector2 target, float time, int type, GameObject targetObject)
-    // // 1 is for enter the screen, 2 is for exit the screen
-    // {
-    //     RectTransform rectTransform = targetObject.GetComponent<RectTransform>();
-    //     Vector2 startPos = rectTransform.anchoredPosition;
-    //     float elapsedTime = 0;
+    private IEnumerator MoveRoutine(Vector2 target, float time, int type, GameObject targetObject)
+    // 1 is for enter the screen, 2 is for exit the screen
+    {
+        RectTransform rectTransform = targetObject.GetComponent<RectTransform>();
+        Vector2 startPos = rectTransform.anchoredPosition;
+        float elapsedTime = 0;
 
-    //     while (elapsedTime < time)
-    //     {
-    //         // Calculate how far along we are (0.0 to 1.0)
-    //         float t = elapsedTime / time;
+        while (elapsedTime < time)
+        {
+            // Calculate how far along we are (0.0 to 1.0)
+            float t = elapsedTime / time;
 
-    //         // Optional: Make it "Smooth" (Ease In and Out) instead of robotic linear speed
-    //         // If you want constant speed, remove this line.
-    //         t = Mathf.SmoothStep(0.0f, 1.0f, t);
+            // Optional: Make it "Smooth" (Ease In and Out) instead of robotic linear speed
+            // If you want constant speed, remove this line.
+            t = Mathf.SmoothStep(0.0f, 1.0f, t);
 
-    //         // Move the image
-    //         rectTransform.anchoredPosition = Vector2.Lerp(startPos, target, t);
+            // Move the image
+            rectTransform.anchoredPosition = Vector2.Lerp(startPos, target, t);
 
-    //         elapsedTime += Time.deltaTime;
-    //         yield return null; // Wait for the next frame
-    //     }
+            elapsedTime += Time.deltaTime;
+            yield return null; // Wait for the next frame
+        }
 
-    //     // Ensure it lands exactly on the target at the end
-    //     rectTransform.anchoredPosition = target;
-    //     if (type == 1)
-    //     {
-    //         noticeMessage.SetActive(true);
-    //     }
-    //     else if (type == 2)
-    //     {
-    //         noticeMessage.SetActive(false);
-    //     }
-    //     else if (type == 3)
-    //     {
-    //         modeAddedNoticeMessage.SetActive(true);
-    //     }
-    //     else if (type == 4)
-    //     {
-    //         modeAddedNoticeMessage.SetActive(false);
-    //     }
+        // Ensure it lands exactly on the target at the end
+        rectTransform.anchoredPosition = target;
+        if (type == 1)
+        {
+            noticeMessage.SetActive(true);
+        }
+        else if (type == 2)
+        {
+            noticeMessage.SetActive(false);
+        }
+        else if (type == 3)
+        {
+            modeAddedNoticeMessage.SetActive(true);
+        }
+        else if (type == 4)
+        {
+            modeAddedNoticeMessage.SetActive(false);
+        }
 
 
 
-    // }
+    }
 }
